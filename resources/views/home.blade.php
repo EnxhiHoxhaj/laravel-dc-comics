462524be539b0f8b11f4f500003a6758e08ee563
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-09-16 19:45:10 +0200

    feat: page preparation and list for adding data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,41 @@
 {{-- questa view estende il file main.blade.php che è dentro la cartella view/layouts --}}
 @extends('layouts.main')
 
-@section('hero')
-    <div>HOME</div>
-@endsection
 
 @section('content')
 <div class="container my-5">
-    <h1>Titolo</h1>
-    <img src="{{ Vite::asset('resources/img/colibri.jpg') }}" alt="" class="img-fluid">
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque impedit dolor recusandae nobis nulla vero, culpa odit blanditiis autem numquam laudantium consectetur dolorum illo quam nisi cumque eius. Iusto, nulla.
-    </p>
+    <h1>Dc Comics</h1>
+    <!-- On tables -->
+    <table class="table-primary">...</table>
+    <table class="table-secondary">...</table>
+    <table class="table-success">...</table>
+    <table class="table-danger">...</table>
+    <table class="table-warning">...</table>
+    <table class="table-info">...</table>
+    <table class="table-light">...</table>
+    <table class="table-dark">...</table>
+
+    <!-- On rows -->
+    <tr class="table-primary">...</tr>
+    <tr class="table-secondary">...</tr>
+    <tr class="table-success">...</tr>
+    <tr class="table-danger">...</tr>
+    <tr class="table-warning">...</tr>
+    <tr class="table-info">...</tr>
+    <tr class="table-light">...</tr>
+    <tr class="table-dark">...</tr>
+
+    <!-- On cells (`td` or `th`) -->
+    <tr>
+    <td class="table-primary">...</td>
+    <td class="table-secondary">...</td>
+    <td class="table-success">...</td>
+    <td class="table-danger">...</td>
+    <td class="table-warning">...</td>
+    <td class="table-info">...</td>
+    <td class="table-light">...</td>
+    <td class="table-dark">...</td>
+    </tr>
 </div>
 
 @endsection
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,9 +1,5 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Layout base</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+<nav class="navbar navbar-expand-lg bg-body-tertiary navb">
+    <div class="container-fluid ">
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
             <a class="nav-link" href="{{ route('home') }}">Home</a>
