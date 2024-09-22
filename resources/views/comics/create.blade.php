47134a2520c551e7cd7ae560af7aec64a292dacf
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-09-22 22:52:03 +0200

    feat: prints all errors on the page

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -12,7 +12,12 @@
 
         @if ($errors->any())
             <div class="alert alert-danger" role="alert">
-                errori...
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+
+                </ul>
             </div>
         @endif
 
